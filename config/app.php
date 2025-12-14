20a0770c32b8f44d369b5b0d598f6e4fbc264ae9
anupdeveloper <anupdeveloper07@gmail.com>
2025-12-14 22:57:49 +0530

    fiixng

--- config/app.php
+++ config/app.php
@@ -235,7 +235,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         //Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
-
+        Spatie\CookieConsent\CookieConsentServiceProvider::class,
     ],
 
     /*
