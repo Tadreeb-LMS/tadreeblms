80357a470e60947932c0742ea93b8c2caa6bda32
krupa-1926 <patelkrupa1926@gmail.com>
2026-02-26 12:18:00 +0530

    Show course start date and expiry date in courses list table #109

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -68,7 +68,28 @@ class Course extends Model
         });
     }
 
+public function setExpiryDateAttribute($input)
+{
+    if ($input != null && $input != '') {
+        $this->attributes['expiry_date'] =
+            Carbon::createFromFormat(config('app.date_format'), $input)
+                ->format('Y-m-d');
+    } else {
+        $this->attributes['expiry_date'] = null;
+    }
+}
 
+public function getExpiryDateAttribute($input)
+{
+    $zeroDate = str_replace(['Y', 'm', 'd'], ['0000', '00', '00'], config('app.date_format'));
+
+    if ($input != $zeroDate && $input != null) {
+        return Carbon::createFromFormat('Y-m-d', $input)
+            ->format(config('app.date_format'));
+    } else {
+        return '';
+    }
+}
 
     public function latestModuleWeightage()
     {
