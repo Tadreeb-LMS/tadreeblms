893d160714c8d9d3c87a96d3e799d9fd79553300
krupa-1926 <patelkrupa1926@gmail.com>
2026-03-03 16:31:07 +0530

    Automatically unpublish courses after expiry date #110

--- database/migrations/2026_03_03_122335_add_expiry_date_to_courses_table.php
+++ database/migrations/2026_03_03_122335_add_expiry_date_to_courses_table.php
@@ -10,15 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {   
          Schema::table('courses', function (Blueprint $table) {
             if (!Schema::hasColumn('courses', 'start_date')) {
-                $table->date('start_date')->nullable()->after('published');
+                // $table->date('start_date')->nullable()->after('published');
+                  $table->date('start_date')->nullable()->after('status');
+        // $table->date('expiry_date')->nullable()->after('start_date');
             }
             if (!Schema::hasColumn('courses', 'expiry_date')) {
                 $table->date('expiry_date')->nullable()->after('start_date');
             }
         });
+        
     }
 
     /**
@@ -27,7 +30,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-        $table->dropColumn(['start_date', 'expiry_date']);
-    });
+            //
+        });
     }
 };
