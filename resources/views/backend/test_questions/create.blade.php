b8b731b14c3010587bed7885cfcb0d1ada34fbed
Zheyon <110686204+Zhey-on@users.noreply.github.com>
2026-05-17 16:57:44 +0200

    Fix question creation roadmap UX

--- resources/views/backend/test_questions/create.blade.php
+++ resources/views/backend/test_questions/create.blade.php
@@ -450,56 +450,67 @@
         {{-- /question-type-row --}}
 
 
-        <div class="row">
-          <div class="col-12 col-md-6 mt-3 notextarea"> 
-                <label>Question <span style="color:red">*</span></label>
-                <textarea class="form-control editor" rows="3" name="question" id="question" required="required" data-collapsible-toolbar="1" oninvalid="this.setCustomValidity('Question is required')" oninput="this.setCustomValidity('')"></textarea>
+        @php($isShortAnswerRoadmap = false)
+        <div class="question-roadmap" data-question-roadmap>
+            <div class="question-roadmap-steps" aria-label="Question builder roadmap">
+                <button type="button" class="question-roadmap-step is-active" data-roadmap-target="question">
+                    <span class="question-roadmap-step-number">1</span>
+                    Question
+                </button>
+                <button type="button" class="question-roadmap-step {{ $isShortAnswerRoadmap ? 'is-hidden' : '' }}" data-roadmap-target="options">
+                    <span class="question-roadmap-step-number">2</span>
+                    Options
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="marks">
+                    <span class="question-roadmap-step-number">3</span>
+                    Marks
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="solution">
+                    <span class="question-roadmap-step-number">4</span>
+                    Solution
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="comment">
+                    <span class="question-roadmap-step-number">5</span>
+                    Comment
+                </button>
             </div>
-         
-                <div class="col-12 col-md-6"> 
-                     <div class="mt-3 notextarea">
-                    <label><i class="fa fa-check-square-o mr-2" style="color: #4e73df;"></i>Opzioni</label>
-                    <textarea class="form-control editor" rows="3" name="option" id="option" required="required" data-collapsible-toolbar="1" placeholder="Scrivi l'opzione di risposta qui..."></textarea>
-                    <div class="addoptbtn mt-3">
-                        <button type="button" id="add_option" class="btn btn-primary btn-lg w-100" style="font-weight: 600; padding: 12px;">
-                            <i class="fa fa-plus-circle mr-2"></i>Aggiungi Opzione
-                        </button>
+
+            <div class="question-roadmap-content">
+                <fieldset class="question-roadmap-panel notextarea is-active" data-roadmap-panel="question">
+                    <legend>
+                        <span class="question-roadmap-panel-icon"><i class="fa fa-question-circle"></i></span>
+                        Question <span style="color:red">*</span>
+                    </legend>
+                    <div class="question-roadmap-panel-body">
+                        <textarea class="form-control editor" rows="3" name="question" id="question" required="required" data-collapsible-toolbar="1" oninvalid="this.setCustomValidity('Question is required')" oninput="this.setCustomValidity('')"></textarea>
                     </div>
-              <div class="addoptiontable mt-4">
-                    <div id="option-area" class=""></div>
-                </div>
-               </div>
+                </fieldset>
+
+                <fieldset class="question-roadmap-panel notextarea {{ $isShortAnswerRoadmap ? 'is-hidden' : '' }}" data-roadmap-panel="options">
+                    <legend>
+                        <span class="question-roadmap-panel-icon"><i class="fa fa-check-square-o"></i></span>
+                        Options
+                    </legend>
+                    <div class="question-roadmap-panel-body">
+                        <textarea class="form-control editor" rows="3" name="option" id="option" required="required" data-collapsible-toolbar="1" placeholder="Scrivi l'opzione di risposta qui..."></textarea>
+                        <div class="addoptbtn mt-3">
+                            <button type="button" id="add_option" class="btn btn-primary btn-lg w-100" style="font-weight: 600; padding: 12px;">
+                                <i class="fa fa-plus-circle mr-2"></i>Aggiungi Opzione
+                            </button>
+                        </div>
+                        <div class="addoptiontable mt-4">
+                            <div id="option-area" class=""></div>
+                        </div>
+                    </div>
+                </fieldset>
+
+                @include('backend.test_questions.partials.footer-fields')
             </div>
-</div>
 
-
-            <div class="row">
-                 <div class="col-12 col-md-5 notextarea">
-                    <label>Solution</label>
-                    <textarea class="form-control textarea-col editor" rows="3" name="solution" id="solution" data-collapsible-toolbar="1"></textarea>
-                </div>
-             
-             <div class="col-12 col-md-2">
-                    <label>Marks <span style="color:red">*</span></label>
-                    <input type="number"
-                        class="form-control"
-                        name="score"
-                        id="score"
-                        placeholder="Enter Marks"
-                        min="1"
-                        max="999"
-                        oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0,3);"
-                        required />
-                </div>
-             
-                <div class="col-12 col-md-5 notextarea">
-                    <label>Comment</label>
-                    <textarea class="form-control textarea-col editor" rows="3" name="comment" id="comment" data-collapsible-toolbar="1"></textarea>
-
-         
-
-         
-        </div>
+            <div class="question-roadmap-actions">
+                <button type="button" class="btn cancel-btn question-roadmap-prev">Back</button>
+                <button type="button" class="btn add-btn question-roadmap-next">Continue</button>
+            </div>
         </div>
 
 
--- resources/views/backend/test_questions/edit.blade.php
+++ resources/views/backend/test_questions/edit.blade.php
@@ -68,79 +68,67 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="mt-3 notextarea">
-                <label>Question </label>
-                <textarea class="form-control editor" rows="3" name="question" id="question" value="" required="required">@if(isset($question->id)){{$question->question_text}}@endif</textarea>
+        @php($isShortAnswerRoadmap = isset($question) && (int) $question->question_type === 3)
+        <div class="question-roadmap" data-question-roadmap>
+            <div class="question-roadmap-steps" aria-label="Question builder roadmap">
+                <button type="button" class="question-roadmap-step is-active" data-roadmap-target="question">
+                    <span class="question-roadmap-step-number">1</span>
+                    Question
+                </button>
+                <button type="button" class="question-roadmap-step {{ $isShortAnswerRoadmap ? 'is-hidden' : '' }}" data-roadmap-target="options">
+                    <span class="question-roadmap-step-number">2</span>
+                    Options
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="marks">
+                    <span class="question-roadmap-step-number">3</span>
+                    Marks
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="solution">
+                    <span class="question-roadmap-step-number">4</span>
+                    Solution
+                </button>
+                <button type="button" class="question-roadmap-step" data-roadmap-target="comment">
+                    <span class="question-roadmap-step-number">5</span>
+                    Comment
+                </button>
             </div>
- </div>
 
-            <div class="col-12 col-md-6 ">
- <div class="mt-3 notextarea">
-               
-                    <label>Option</label>
-                    <textarea class="form-control editor" rows="3" name="option" id="option" required="required"></textarea>
-                    <div class="addoptbtn">
-                    <button type="button" id="add_option" class="btn btn-primary">Add Option</button>
+            <div class="question-roadmap-content">
+                <fieldset class="question-roadmap-panel notextarea is-active" data-roadmap-panel="question">
+                    <legend>
+                        <span class="question-roadmap-panel-icon"><i class="fa fa-question-circle"></i></span>
+                        Question
+                    </legend>
+                    <div class="question-roadmap-panel-body">
+                        <textarea class="form-control editor" rows="3" name="question" id="question" value="" required="required">@if(isset($question->id)){{$question->question_text}}@endif</textarea>
                     </div>
-                    <div class="addoptiontable ">
-                         <div id="option-area"></div>
+                </fieldset>
+
+                <fieldset class="question-roadmap-panel notextarea {{ $isShortAnswerRoadmap ? 'is-hidden' : '' }}" data-roadmap-panel="options">
+                    <legend>
+                        <span class="question-roadmap-panel-icon"><i class="fa fa-check-square-o"></i></span>
+                        Options
+                    </legend>
+                    <div class="question-roadmap-panel-body">
+                        <textarea class="form-control editor" rows="3" name="option" id="option" required="required"></textarea>
+                        <div class="addoptbtn">
+                            <button type="button" id="add_option" class="btn btn-primary">Add Option</button>
                         </div>
-              </div>
-             
+                        <div class="addoptiontable">
+                            <div id="option-area"></div>
+                        </div>
+                    </div>
+                </fieldset>
 
+                @include('backend.test_questions.partials.footer-fields', ['question' => $question ?? null])
+            </div>
 
-        </div>
-
-
-
-
-        <!-- <div class="cb_question_setup">
-            
-            
-
-                <div class="col-6">
-                    
-                    
-                   
-                    
-                </div>
-            </div> -->
-            
+            <div class="question-roadmap-actions">
+                <button type="button" class="btn cancel-btn question-roadmap-prev">Back</button>
+                <button type="button" class="btn add-btn question-roadmap-next">Continue</button>
+            </div>
         </div>
     </div>
-
-
-
-
-<div class="row">
-                <div class="col-12 col-md-5 notextarea">
-                    <label>Solution</label>
-                    <textarea class="form-control textarea-col editor" rows="3" name="solution" id="solution" value="$question->solution">@if(isset($question->id)){{$question->solution}}@endif</textarea>
-                </div>
-            
-                <div class="col-12 col-md-2">
-                    <label>Marks <span style="color:red">*</span></label>
-                    <input
-                        type="number"
-                        class="form-control"
-                        name="score"
-                        id="score"
-                        placeholder="Enter Marks"
-                        required
-                        value="{{$question->score}}"
-                        min="1"
-                        max="999"
-                        oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0,3);"
-                    />
-                </div>
-            
-                <div class="col-12 col-md-5 notextarea">
-                    <label>Comment</label>
-                    <textarea class="form-control textarea-col editor" rows="3" name="comment" id="comment">@if(isset($question->id)){{$question->comment}}@endif</textarea>
-                </div>
-            </div>
 
 
 
