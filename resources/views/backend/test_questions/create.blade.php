9f07b9deaaf5d8be99b736b12c32c5e74dc225e0
sayanwis06 <112060386+sayanwis06@users.noreply.github.com>
2026-07-21 14:04:50 +0530

    Revert "Incorrect Action Buttons Displayed in Lesson Creation Form #910 (#912)" (#922)

--- resources/views/backend/test_questions/create.blade.php
+++ resources/views/backend/test_questions/create.blade.php
@@ -525,39 +525,21 @@
      <button type="button" class="frm_submit add-btn" id="save_and_add_more" value="save_and_add_more">Save & Add More</button>
 
      <span class="text-right pull-right">
-        @if($isWizard)
-                            <div class="d-flex align-items-center">
-                                <button type="submit"
-                                    class="btn cancel-btn frm_submit mr-2"
-                                    id="doneBtn">
-                                    Save As Draft
-                                </button>
+        <button
+            type="button"
+            class="frm_submit cancel-btn"
+            id="save_as_draft"
+            value="Save As Draft">
+            Save As Draft
+        </button>
 
-                                <button type="submit"
-                                    class="btn add-btn frm_submit"
-                                    id="nextBtn">
-                                    Next
-                                </button>
-                            </div>
-                            @else
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        type="button"
-                                        class="btn btn-outline-secondary mr-2 cancel-btn"
-                                        onclick="window.location='{{ route('admin.test_questions.index') }}'">
-                                        Cancel
-                                    </button>
-
-                                    <button
-                                        type="submit"
-                                        class="btn add-btn frm_submit"
-                                        id="saveBtn">
-                                        Save
-                                    </button>
-
-                                    <span class="loading ml-3"></span>
-                                </div>
-                        @endif
+        <button
+            type="button"
+            class="frm_submit add-btn"
+            id="save"
+            value="Next">
+            Next
+        </button>
 </span>
     </div>
 
