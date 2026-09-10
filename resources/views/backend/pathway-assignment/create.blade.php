dbc182c4bd66f2f2147e766baacbd446489f4a42
Krupa Patel <krupapatel1926@gmail.com>
2026-09-10 17:01:08 +0530

    Outdated Label Select Department in Create Assignment #957 (#1024)
    Co-authored-by: krupa-1926 <patelkrupa1926@gmail.com>

--- resources/views/backend/pathway-assignment/create.blade.php
+++ resources/views/backend/pathway-assignment/create.blade.php
@@ -127,11 +127,11 @@
 
                 {{-- Department --}}
                 <div class="row">
-                    <label class="col-md-12 form-control-label">@lang('Select Department')</label>
+                    <label class="col-md-12 form-control-label">@lang('Select User Group')</label>
                     <div class="col-md-12">
                         <div class="custom-select-wrapper position-relative">
                             <select name="department_id" class="form-control select2">
-                                <option value="" selected disabled>@lang('Select Department')</option>
+                                <option value="" selected disabled>@lang('Select User Group')</option>
                                 @foreach ($departments as $row)
                                     <option value="{{ $row->id }}" {{ old('department_id') == $row->id ? 'selected' : '' }}>
                                         {{ $row->title }}
